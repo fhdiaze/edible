64e34ef2ee7fb8bbb3b9ca3c804a19fd528b51bb
fredy.diaz <fredy.diaz@2degrees.nz>
2024-10-19 13:38:30 +1300

    feat: add pages

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 use askama::Template;
 use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+  tracing_subscriber::fmt::init();
 
-    let router = Router::new().route("/", get(handle_get));
+  info!("Starting the web server!");
 
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+  let router = Router::new()
+    .nest("/", route_home())
+    .nest("/", route_school())
+    .nest("/", route_blog())
+    .nest("/", route_about())
+    .nest("/", route_contact())
+    .nest("/assets", route_assets());
 
-    axum::serve(listener, router).await.unwrap();
+  let address = SocketAddr::from(([0, 0, 0, 0], 7878));
+  let listener = TcpListener::bind(address)
+    .await
+    .expect("Failed to bind the address!");
+
+  info!("Web server is going to listen on address={}", address);
+
+  axum::serve(listener, router)
+    .await
+    .expect("Failed to start the web server");
+}
+
+fn route_about() -> Router {
+  Router::new().route("/about", get(handle_about))
 }
 
 #[derive(Template)]
@@ -18,8 +40,72 @@ async fn main() {
 struct HomeTemplate;
 
 async fn handle_get() -> Html<String> {
-    let template = HomeTemplate {};
-    let content = template.render().expect("msg");
+  let template = HomeTemplate {};
+  let content = template.render().expect("msg");
 
-    Html(content)
+  Html(content)
+}
+
+#[derive(Template)]
+#[template(path = "school.html")]
+struct SchoolTemplate;
+
+async fn handle_school() -> Html<String> {
+  let template = SchoolTemplate {};
+  let content = template.render().expect("error!!");
+
+  Html(content)
+}
+
+#[derive(Template)]
+#[template(path = "about.html")]
+struct AboutTemplate;
+
+async fn handle_about() -> Html<String> {
+  let template = AboutTemplate {};
+  let content = template.render().expect("error!!");
+
+  Html(content)
+}
+
+fn route_home() -> Router {
+  Router::new().route("/", get(handle_get))
+}
+
+fn route_school() -> Router {
+  Router::new().route("/school", get(handle_school))
+}
+
+fn route_assets() -> Router {
+  Router::new().nest_service("/", ServeDir::new("assets"))
+}
+
+#[derive(Template)]
+#[template(path = "blog.html")]
+struct BlogTemplate;
+
+async fn handle_blog() -> Html<String> {
+  let template = BlogTemplate {};
+  let content = template.render().expect("error!!");
+
+  Html(content)
+}
+
+fn route_blog() -> Router {
+  Router::new().route("/blog", get(handle_blog))
+}
+
+fn route_contact() -> Router {
+  Router::new().route("/contact", get(handle_contact))
+}
+
+#[derive(Template)]
+#[template(path = "contact.html")]
+struct ContactTemplate;
+
+async fn handle_contact() -> Html<String> {
+  let template = ContactTemplate {};
+  let content = template.render().expect("error!!");
+
+  Html(content)
 }
